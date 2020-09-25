c887426ef39df7c78a4fb397d334dda561678a09
David Qian <lachine2008@gmail.com>
2020-09-25 13:43:26 -0700

    Update flags.js

--- src/utils/flags.js
+++ src/utils/flags.js
@@ -1,8 +1,8 @@
 import Rox from 'rox-browser'
 
 export const Flags = {
-  sidebar: new Rox.Flag(false),
-  title: new Rox.Flag(false)
+  sidebar: new Rox.Flag(true),
+  title: new Rox.Flag(true)
 }
 
 async function initRollout () {
